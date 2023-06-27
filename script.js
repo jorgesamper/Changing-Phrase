9a039a4ddb07315e02f0b0806bca43e43679cc38
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-28 00:30:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function(){
+      $(".typed").typed({
+        strings:["transformo ideas en experiencias digitales.", "diseño interfaces.", "optimizo interacción usuario-producto."],
+        typeSpeed:1,
+        loop: true,
+        backDelay:1000
+        });
+    });  
